a692bb83498818e44353bdd13bd131b3efa9402d
jimgrind <benjaminisaacgreen@gmail.com>
2022-08-20 18:22:04 -0700

    Added Networking Base Class

--- NetworkManager.cpp
+++ NetworkManager.cpp
@@ -1,20 +1,18 @@
-/*
+
 #include "NetworkManager.h"
 
 
 // Probably should add exceptions here
-NetworkManager::NetworkManager(int port = 25565) : port(port) {
+NetworkManager::NetworkManager(const int port = 25565, const char * ip = "") : port(port), ip(ip) {
 	winsock_init();
 	sock_bind();
-
-
 }
 
 NetworkManager::~NetworkManager() {
-	winsock_cleanup();
-
-	// Go through and force close all client sockets/threads
-	//
+	// Close socket
+	closesocket(sock);
+	// Shutdown Winsock
+	WSACleanup();
 }
 
 
@@ -30,22 +28,30 @@ int NetworkManager::winsock_init() {
 	}
 
 	return 0;
-
-}
-
-void winsock_cleanup() {
-
 }
 
 int NetworkManager::sock_bind() {
 
-	serverSock = socket(AF_INET, SOCK_DGRAM, 0);
-	serverHint.sin_addr.S_un.S_addr = ADDR_ANY;
-	serverHint.sin_family = AF_INET;
-	serverHint.sin_port = htons(port);
+	// Server local_sockaddrs for UDP and designated port
+	local_sockaddr.sin_family = AF_INET;
+	local_sockaddr.sin_port = htons(port);
 
-	int err =  bind(serverSock, (sockaddr*)&serverHint, sizeof(serverHint));
+	// Clients will use this to find the server by IP
+	if (strlen(ip) > 0) {
+		local_sockaddr.sin_addr.S_un.S_addr = inet_addr(ip);
+		// inetpton(AF_INET, ip, &sock.sin_addr);
+	}
+	else {
+		local_sockaddr.sin_addr.S_un.S_addr = ADDR_ANY;
+	}
 
+	// Create Socket
+	sock = socket(AF_INET, SOCK_DGRAM, 0);
+
+	// Bind to socket
+	int err =  bind(sock, (sockaddr*)&local_sockaddr, sizeof(local_sockaddr));
+
+	// Handle Errors I guess
 	if (err == SOCKET_ERROR) {
 		std::cout << "Can't bind Socket " << WSAGetLastError() << std::endl;
 		return err;
@@ -53,4 +59,51 @@ int NetworkManager::sock_bind() {
 
 	return 0;
 }
-*/
+
+packet * NetworkManager::recv(bool blocking, sockaddr_in * from) {
+
+	int fromLen = sizeof(from);
+
+	ZeroMemory(from, fromLen);
+	ZeroMemory(buf, MAX_PACK_BYTES);
+
+	packet* pack = new packet;
+	int bytesIn = 0;
+	do {
+
+
+		recvfrom(sock, buf, MAX_PACK_BYTES, 0, (sockaddr*)from, &fromLen);
+		if (bytesIn == SOCKET_ERROR) {
+			std::cout << "Error receiving message : " << WSAGetLastError() << std::endl;
+			continue; // TODO: Better error handling
+		}
+
+	} while(blocking);
+
+	char clientIp[256];
+	ZeroMemory(clientIp, 256);
+
+	inet_ntop(AF_INET, &other_sockaddr.sin_addr, clientIp, 256);
+
+	std::cout << "Message recv from " << clientIp << " : " << buf << std::endl;
+
+	return pack;
+}
+
+int NetworkManager::send(const char * buf, sockaddr_in* to) {
+
+	int toLen = sizeof(to);
+
+	ZeroMemory(to, toLen);
+
+	int sendOk = sendto(sock, buf, MAX_PACK_BYTES, 0, (sockaddr*)to, toLen);
+
+	if (sendOk == SOCKET_ERROR) {
+		std::cout << "Error sending message : " << WSAGetLastError() << std::endl;
+		return sendOk; // TODO: Better error handling
+	}
+	else {
+		std::cout << "Sent message to other_sockaddr : " << buf << std::endl;
+		return 0;
+	}
+}
--- NetworkManager.h
+++ NetworkManager.h
@@ -1,31 +1,39 @@
-/*
+
 #pragma once
 
 #include <iostream>
 #include <WS2tcpip.h>
 #include <array>
 
-#define MAX_CLIENTS 8
+#pragma comment(lib,"ws2_32.lib") //Winsock Library
+
+#define MAX_PACK_BYTES 1024
+
+struct packet {
+	
+};
 
 class NetworkManager {
 private:
-	// Clients and Server Objects should be here
-	// Threads should be here/inside client/server objects
-
-	std::array<int, MAX_CLIENTS> clients;
-
+	
 	const int port;
-	SOCKET serverSock;
-	sockaddr_in serverHint;
+	const char* ip;
+	SOCKET sock;
+	sockaddr_in local_sockaddr;
+	sockaddr_in other_sockaddr;
 
 	int sock_bind();
 	int winsock_init();
 
 public:
 
-	NetworkManager(int);
+	char * recv(bool, sockaddr_in *);
+	int send(char *, int, sockaddr_in *);
+	int connectTo(char *)
+
+	NetworkManager(const int);
 	~NetworkManager();
 
 };
-*/
+
 
