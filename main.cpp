08f67fd618de856d8b6ec953c727a91bc6a3367c
jimgrind <benjaminisaacgreen@gmail.com>
2022-12-04 13:55:21 -0800

    Switched over to cmake and vscode

--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
+
 #include <iostream>
 #include <fstream>
-#include "glad\glad.h"
+#include "glad/glad.h"
 #define GLFW_INCLUDE_NONE
-#include "GLFW\glfw3.h"
-#include <GL\GL.h>
-#include "glm\glm.hpp"
-#include "glm\gtc\matrix_transform.hpp"
+#include "GLFW/glfw3.h"
+#include <GL/GL.h>
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
 #include "glm/gtc/type_ptr.hpp"
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb_image.h>
